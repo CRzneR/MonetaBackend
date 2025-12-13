54a6507cb7107fb58cd55d23b566133e1a84185a
CRzneR <renzchris@web.de>
2025-12-13 18:53:39 +0100

    implemented save function

--- models/Cost.js
+++ models/Cost.js
@@ -2,10 +2,11 @@ import mongoose from "mongoose";
 
 const costSchema = new mongoose.Schema(
   {
-    kosten: Number,
-    name: String,
-    kategorie: String,
-    costType: String, // fix, jährlich, variabel
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    kosten: { type: Number, required: true },
+    name: { type: String, required: true },
+    kategorie: { type: String, required: true },
+    costType: { type: String, required: true }, // fix, jährlich, variabel
   },
   { timestamps: true }
 );
