e48ba70c223ca01442cfc78eb4b9d086cde42187
CRzneR <renzchris@web.de>
2026-04-08 18:06:10 +0200

    table fix

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-  })
+  }),
 );
 app.options("*", cors());
 
@@ -42,12 +42,12 @@ app.use("/api/income", incomeRoutes);
 
 // ROOT Route → optional nur lokal relevant
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/fixkosten.html"));
+  res.sendFile(path.join(__dirname, "../pages/frontend/fixkosten.html"));
 });
 
 app.get("*", (req, res, next) => {
   if (req.originalUrl.startsWith("/api")) return next();
-  res.sendFile(path.join(__dirname, "../frontend/fixkosten.html"));
+  res.sendFile(path.join(__dirname, "../pages/frontend/fixkosten.html"));
 });
 
 // MongoDB Verbindung & Serverstart
